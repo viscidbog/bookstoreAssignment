659b478e325a18a79f1e69060df5d74244fb6510
viscidbog <124368715+viscidbog@users.noreply.github.com>
2024-02-05 13:19:49 +0200

    first commit

---
+++ src/main/java/hh/tehtava/kirjakauppa/KirjakauppaApplication.java
@@ -0,0 +1,13 @@
+package hh.tehtava.kirjakauppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirjakauppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirjakauppaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hh/tehtava/kirjakauppa/KirjakauppaApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.tehtava.kirjakauppa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirjakauppaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
